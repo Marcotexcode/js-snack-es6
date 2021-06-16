36362da491aa5aec201e67b78431cd40f8aa7766
Marco Tessitore <marcotexcode@gmail.com>
2021-06-16 14:33:46 +0200

    step generare numero random con arrow

--- esercizio2/js/script.js
+++ esercizio2/js/script.js
@@ -7,3 +7,45 @@
 
 
 
+
+// CREARE UN ARRAY CON OGGETTI DI SQUADRE CON PROPRIETA: NOME, PUNTI, FALLI
+
+const squadre = [
+
+    {nome: 'Juventus', punti: 0, falli: 0},
+    {nome: 'Milan', punti: 0, falli: 0},
+    {nome: 'Inter', punti: 0, falli: 0},
+    {nome: 'Napoli', punti: 0, falli: 0},
+    {nome: 'Fiorentina', punti: 0, falli: 0},
+    {nome: 'Pescara', punti: 0, falli: 0}
+
+]
+
+// CREARE UN CICLO CHE MODIFICHI I VALORI: PUNTI, FALLI, DI OGNI OGGETTO
+
+for( let i = 0; i < squadre.length; i++) {
+
+    const {punti, peso} = squadre[i];
+
+    console.log(squadre[i]);
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// GENERARE UN NUMERO RANDOM AL POSTO DELLE PROPRIETA A 0 UTILIZZANDO ARROW FUNCTION
+
+const numeriRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+
+
